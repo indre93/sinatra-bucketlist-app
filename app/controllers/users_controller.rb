1ae2ec36be188ebf55b2c3aff6861e79d55d5184
indre93 <indre.bakeer@gmail.com>
2020-02-24 18:30:06 -0600

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class UsersController < ApplicationController
  
   # The purpose of this route is to render the login page (form)
@@ -5,9 +7,26 @@ class UsersController < ApplicationController
     erb :login
   end
 
-  # The purpose of this route is to render the signup page (form)
-  get '/signup' do 
-  
+  # The purpose of this route is to receive the login form, find the user and log the user in (create a session)
+  post '/login' do 
+    #find the user
+    @user = User.find_by(email: params[:email])
+    #authenticate the user (verified the user is who they say they are - have the right credentials)
+    if @user.authenticate(params[:password])
+    #log the user in - create the user session
+      session[:user_id] = @user.id #actually logging the user in
+    #redirect the user's landing page (user show page)
+      puts session
+      redirect "users/#{@user.id}"
+    else
+      #tell the user the have entered the invalid credential and redirect them to the login page
+    end
   end
+
+  # user SHOW route
+  get '/users/:id' do
+    "this will be the user show route"
+  end
+
 
 end
