7e16ce0a754fab00a2a060352e82f3cadf760123
indre93 <indre.bakeer@gmail.com>
2020-02-24 18:41:41 -0600

    add helper methods - logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,17 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+
+    def logged_in?
+      # true if user logged in, otherwise fale
+      !!current_user 
+    end
+
+    def current_user
+      # this instance variable will be created and assigned if user found, otherwise it will still be nill
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
 end
