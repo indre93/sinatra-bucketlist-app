03a8fdf266261f6f92c2e5988b3f11ace45ec6b9
indre93 <indre.bakeer@gmail.com>
2020-02-28 14:03:07 -0600

    add signup and users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,34 @@ class UsersController < ApplicationController
 
   # The purpose of this route is to receive the login form, find the user and log the user in (create a session)
   post '/login' do 
-    #find the user
+    # find the user
     @user = User.find_by(email: params[:email])
-    #authenticate the user (verified the user is who they say they are - have the right credentials)
+    # authenticate the user (verified the user is who they say they are - have the right credentials)
     if @user.authenticate(params[:password])
-    #log the user in - create the user session
+    # log the user in - create the user session
       session[:user_id] = @user.id #actually logging the user in
-    #redirect the user's landing page (user show page)
+    # redirect the user's landing page (user show page)
       puts session
       redirect "users/#{@user.id}"
     else
-      #tell the user the have entered the invalid credential and redirect them to the login page
+      # tell the user the have entered the invalid credential
+      # redirect them to the login page
+    end
+  end
+
+  # this route's job is to render the signup page
+  get '/signup' do
+    erb :signup
+  end
+
+  # this is where we will create the new user and persist the new user to the database
+  post '/users' do
+    # I only want to persist a user that has a name, email & password
+    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+      # valid input
+      @user = User.create(params)
+    else
+      # not valid input
     end
   end
 
