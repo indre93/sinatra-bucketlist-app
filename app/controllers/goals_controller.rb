7f44c000ee835f15861d5c82eece483d381e52d0
indre93 <indre.bakeer@gmail.com>
2020-02-29 15:23:08 -0600

    add new goal route

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,17 @@
+class GoalsController < ApplicationController
+
+  # this is to render a form to create a new goal
+  get '/goals/new' do
+    erb :'/goals/new'
+  end
+
+  # post goals route to create a new goal
+  post '/goals' do
+
+  end
+
+  # show route for a goal
+
+  # index route to show all goals
+
+end
