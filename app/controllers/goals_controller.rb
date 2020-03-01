099f3315e551b590076bddf045211109a0123c41
indre93 <indre.bakeer@gmail.com>
2020-03-01 14:03:16 -0600

    add private helper method and edit edit route

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,15 +7,13 @@ class GoalsController < ApplicationController
 
   # this route to create a new goal & save it to the DB
   post '/goals' do 
-    # Only want to create if user logged in
     if !logged_in?
       redirect '/'
     end
-
     # Only want to save it if it has some content
     if params[:title] != "" && params[:description] != ""
       @goal = Goal.create(title: params[:title], description: params[:description], user_id: current_user.id)
-      redirect "/goals/#{@goal.id}"
+      redirect "goals/#{@goal.id}"
     else
       redirect '/goals/new'
     end
@@ -23,16 +21,45 @@ class GoalsController < ApplicationController
 
   # show route for a goal
   get '/goals/:id' do
-    @goal = Goal.find_by(params[:id])
+    set_goal 
     erb :'/goals/show'
   end
 
-  # route to edit a goal - should send us to goal/edit.erb
-  # render an edit form
+  # route to edit a goal - should send us to goal/edit.erb & render edit form
   get '/goals/:id/edit' do
-    erb :'/goals/edit'
+    set_goal 
+    if logged_in?
+      if @goal.user == current_user
+        erb :'/goals/edit'
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else 
+      redirect '/'
+    end
+  end
+
+
+  patch '/goals/:id' do 
+    set_goal 
+    if logged_in?
+      if @goal.user == current_user
+        @goal.update(title: params[:title], description: params[:description])
+        redirect "/goals/#{@goal.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else
+      redirect '/'
+    end
   end
 
   # index route to show all goals
+
+  private # it means we're going to create methods that will only be used by this class
+  
+  def set_goal 
+    @goal = Goal.find_by(params[:id])
+  end
 
 end
