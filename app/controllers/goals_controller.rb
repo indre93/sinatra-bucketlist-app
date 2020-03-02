dc9ecbba8505e65a1e2e0e22dacadd05da92ef06
indre93 <indre.bakeer@gmail.com>
2020-03-02 17:41:01 -0600

    add flash errors and messages to communicate user outcome of request made

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,6 +6,7 @@ class GoalsController < ApplicationController
       @goals = Goal.all 
       erb :'goals/index'
     else
+      flash[:errors] = "You must be logged in to view this page. Please log in or sign up."
       redirect '/'
     end
   end
@@ -15,22 +16,20 @@ class GoalsController < ApplicationController
     if logged_in?
       erb :'/goals/new'
     else
+      flash[:errors] = "You must be logged in to create a goal. Please log in or sign up."
       redirect '/'
     end
   end
 
   # route to create a new goal & save it to the DB
   post '/goals' do 
-    if !logged_in?
-      redirect '/'
-    end
     # Only want to save it if it has some content
     if params[:title] != "" && params[:description] != ""
-      flash[:messages] = "Goal successfully created!"
       @goal = Goal.create(title: params[:title], description: params[:description], user_id: current_user.id)
+      flash[:messages] = "Goal has been successfully created!"
       redirect "/goals/#{@goal.id}"
     else
-      flash[:errors] = "Unable to create goal. Please try again."
+      flash[:errors] = "Unable to save goal. Please make sure all fields are filled in and try again."
       redirect '/goals/new'
     end
   end
@@ -48,9 +47,11 @@ class GoalsController < ApplicationController
       if authorized_to_edit?(@goal)
         erb :'/goals/edit'
       else
+        flash[:errors] = "Sorry! you cannot make changes to other people's goals."
         redirect "users/#{current_user.id}"
       end
     else 
+      flash[:errors] = "Sorry! you must be logged in to view this page. Please log in or sign up"
       redirect '/'
     end
   end
@@ -61,11 +62,14 @@ class GoalsController < ApplicationController
     if logged_in?
       if authorized_to_edit?(@goal) && params[:title] != "" && params[:description] != ""
         @goal.update(title: params[:title], description: params[:description])
+        flash[:messages] = "Changes have successfully been made!"
         redirect "/goals/#{@goal.id}"
       else
+        flash[:errors] = "Sorry! you cannot make changes to other people's goals."
         redirect "/users/#{current_user.id}"
       end
     else
+      flash[:errors] = "Sorry! you must be logged in to view this page. Please log in or sign up"
       redirect '/'
     end
   end
@@ -74,8 +78,10 @@ class GoalsController < ApplicationController
     set_goal
     if authorized_to_edit?(@goal)
       @goal.destroy
+      flash[:messages] = "Goal has been deleted."
       redirect '/goals'
     else
+      flash[:errors] = "Sorry! you cannot make changes to other people's goals."
       redirect '/goals'
     end
   end
