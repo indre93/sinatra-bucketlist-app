56a60744e38f38c408bac4ab3b6c07f31c799faa
indre93 <indre.bakeer@gmail.com>
2020-03-02 12:42:04 -0600

    update to current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
 
     def authorized_to_edit?(goal)
