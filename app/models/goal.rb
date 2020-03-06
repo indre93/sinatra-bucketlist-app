e150f414c77b32454863ce818a50595f3325120b
indre93 <indre.bakeer@gmail.com>
2020-03-06 15:54:25 -0600

    adijust format for formatted_created_at method

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,6 +2,6 @@ class Goal < ActiveRecord::Base
   belongs_to :user
   
   def formatted_created_at
-    self.created_at.strftime("%A, %b %d %Y at %l:%M %p")
+    self.created_at.strftime("%A, %m/%d/%Y at %l:%M %p")
   end
 end
