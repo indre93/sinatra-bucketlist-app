e1fe0cda4dcb370f0c8d9519577d5825efe1c9d5
indre93 <indre.bakeer@gmail.com>
2020-03-14 14:38:47 -0500

    clean code to make it easier to read

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :goals
+
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-
-  has_many :goals
 
 end
